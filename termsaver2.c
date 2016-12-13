95e214881de8ef53e1106e835b711eac2021a8ee
Nenrikido <ampy00@live.fr>
2016-12-13 11:37:09 +0100

    Updated

--- termsaver2.c
+++ termsaver2.c
@@ -4,12 +4,7 @@
 #include <string.h>
 #define TAILLE_MAX 1000
 
-int i;
-int j;
-int c;
-int k;
 int l;
-char b;
 
 void sleep(int seconds) //Fonction faisant une pause de x secondes
 {
@@ -55,10 +50,12 @@ char *my_strcat(char *dest, char *src) //Fonction remplaçant strcat pour ce pro
 
 void afficherpbm(char *str, int b)
 {
+	int j;
 	char *str2 = malloc(2 * sizeof(char));
 	str2[0] = str[b];
-	str[1] = '\0';
-	char *name = my_strcat(str2, ".pbm");
+	str2[1] = '\0';
+	char *name = strdup("\0");
+	name = my_strcat(str2, ".pbm");
 	char *chaine = malloc(TAILLE_MAX * sizeof(char));
 	char *chaine2 = malloc(TAILLE_MAX * sizeof(char));
 	FILE* fichier = NULL; //Initialisation de la variable de fichier
@@ -73,107 +70,38 @@ void afficherpbm(char *str, int b)
 	fgets(chaine, TAILLE_MAX, fichier);
 	chaine = my_strcat(chaine, chaine2);
 	fclose(fichier);
-	printf("%s\n",fichier);
+	printf("%s\n",chaine);
 }
 
 int main(int argc, char *argv[])
 {
+	int k;
 	int c = 0;
 	char *temps =  malloc(30 * sizeof(char));
-	char *chaine = malloc(TAILLE_MAX * sizeof(char));
-	char *str = malloc(strlen(temps) * sizeof(char) + 1);
-	time_t t = time(NULL); //Récupération du temps courent
+	time_t t = time(NULL); //Récupération du temps courant
 	strcpy(temps,ctime(&t)); //Transfert du temps courant dans le char "temps"
+	char *str = malloc(strlen(temps) * sizeof(char) + 1);
 	while (c != strlen(temps))
 	{
  		if (c + 12 == 12 || c + 12 == 13 || c + 12 == 15 || c + 12 == 16  || c + 12 == 18 || c + 12 == 19)
 		{
 			str[c] = temps[c + 12];
 		}
+		c++;
 	}
 	str[c] = '\0';
-	while(t!=0) //Boucle infinie
+	while(1) //Boucle infinie
 	{
+		printf("test0");
 		for(l=0;l!=8;l++) //Boucle d'affichage
 		{
+			printf("test1");
 			c = 0;
 			while (str[c])
 			{
 				afficherpbm(str, c);
 				c++;
 			}
-//			.pbm++[0] = a;
-//			fichier = fopen(a,"r");
-//			if(i==0)
-//			{
-//				for(j=0;j!=3;j++)
-//				{
-//					fgets(chaine,TAILLE_MAX, fichier);
-//				}
-//			}
-//			fgets(chaine, TAILLE_MAX, fichier);
-//			fclose(fichier);
-//			.pbm++[0] = b;
-//			fichier = fopen(b,"r");
-//			if(i==0)
-//			{
-//				for(j=0;j!=3;j++)
-//				{
-//					fgets(chaine,TAILLE_MAX, fichier);
-//				}
-//			}
-//			fgets(chaine2, TAILLE_MAX, fichier);
-//			chaine = my_strcat(chaine, chaine2);
-//			fclose(fichier);
-//			.pbm++[0] = c;
-//			fichier = fopen(c,"r");
-//			if(i==0)
-//			{
-//				for(j=0;j!=3;j++)
-//				{
-//					fgets(chaine,TAILLE_MAX, fichier);
-//				}
-//			}
-//			fgets(chaine2, TAILLE_MAX, fichier);
-//			chaine = my_strcat(chaine, chaine2);
-//			fclose(fichier);
-//			.pbm++[0] = d
-//			fichier = fopen(d,"r");
-//			if(i==0)
-//			{
-//				for(j=0;j!=3;j++)
-//				{
-//					fgets(chaine,TAILLE_MAX, fichier);
-//				}
-//			}
-//			fgets(chaine2, TAILLE_MAX, fichier);
-//			chaine = my_strcat(chaine, chaine2);
-//			fclose(fichier);
-//			.pbm++[0] = e
-//			fichier = fopen(e,"r");
-//			if(i==0)
-//			{
-//				for(j=0;j!=3;j++)
-//				{
-//					fgets(chaine,TAILLE_MAX, fichier);
-//				}
-//			}
-//			fgets(chaine2, TAILLE_MAX, fichier);
-//			chaine = my_strcat(chaine, chaine2);
-//			fclose(fichier);
-//			.pbm++[0] = f
-//			fichier = fopen(f,"r");
-//			if(i==0)
-//			{
-//				for(j=0;j!=3;j++)
-//				{
-//					fgets(chaine,TAILLE_MAX, fichier);
-//				}
-//			}
-//			fgets(chaine2, TAILLE_MAX, fichier);
-//			chaine = my_strcat(chaine, chaine2);
-//			fclose(fichier);
-
 		}
 		for(k=0;k!=10;k++)
 		{
