71e99f9aa03e2f6d68060779858b45fc87b9786c
Nenrikido <ampy00@live.fr>
2016-12-13 09:56:32 +0100

    Updated

--- termsaver1.c
+++ termsaver1.c
@@ -1,16 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/ioct1.h>
 #define TAILLE_MAX 1000
+
+struct winsize win;
+int col, lig;
+
+int afficher()
+{
+int a;
+int b;
+int c;
+ioctl(0,TIOCGWINSZ,&win);
+col = win.ws_col;
+a = col/2;
+c = a-strlen(chaine)/2;
+for(i=0;i!=c;i++)
+{
+	print(" ");
+}
+printf("%s",chaine);
+}
+
 int main(int argc, char *argv[])
 {
 	int i;
+	int k;
 	int l;
+	int j = 0;
 	char chaine[TAILLE_MAX];
+	char test[TAILLE_MAX];
 	FILE* fichier = NULL;
-	fichier = fopen("1.pbm", "r");
+	fichier = fopen("2.pbm", "r");
 	if (fichier != NULL)
 	{
+		while (fgets(test, TAILLE_MAX, fichier) != NULL);
+		{
+			j++
+		}
+		ioctl(0,TIOCGWINSZ,&win);
+		lig = win.ws_row;
+		j = lig/2-j/2
+		for(i=0;i!=j;i++)
+		{
+			printf("\n")
+		}
+		fseek(fichier,0,SEEK_SET);
 		for(i=0;i!=3;i++)
 		{
 			fgets(chaine, TAILLE_MAX, fichier);
@@ -26,10 +62,10 @@ int main(int argc, char *argv[])
 				}
 				if (chaine[i]=='0')
 				{
-					chaine[i]='#';
+					chaine[i]=("%c",k != 219);
 				}
 			}
-			printf("%s", chaine);
+			afficher();
 		}
 		fclose(fichier);
 	}
