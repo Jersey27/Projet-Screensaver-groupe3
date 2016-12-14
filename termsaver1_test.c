310acba593e39c4e7a47cd3c85ae9f77898f4604
Jersey27 <tristan.lefevre@viacesi.fr>
2016-12-14 11:20:33 +0100

    rectification du programme

--- termsaver1_test.c
+++ termsaver1_test.c
@@ -1,69 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <termios.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
-
 #define TAILLE_MAX 1000
 
+struct winsize win;
+int col, lig;
+int i;
 char chaine[TAILLE_MAX];
 
+/*int getch(void)
+{
+	struct termios oldattr, newattr;
+	int ch;
+	tcgetattr( STDIN_FILENO, &oldattr);
+	newattr = oldattr;
+	newattr.c_lflag &= ~( ICANON | ECHO);
+	tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+	ch = getchar();
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+	return ch;
+}*/
 int afficher()
 {
 int a;
 int b;
 int c;
-int d;
-int i;
-struct winsize win;
-int col, lig;
-
-ioctl(0,TIOCGWINSZ, &win);
-
+ioctl(0,TIOCGWINSZ,&win);
 col = win.ws_col;
-lig = win.ws_row;
-
 a = col/2;
-b = lig/2;
 c = a-strlen(chaine)/2;
-//d = b-
 for(i=0;i!=c;i++)
 {
 	printf(" ");
 }
 printf("%s",chaine);
-return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	char *arguments[] = {"fichier2", NULL};
-	pid_t pid = getppid();
-	pid = fork(); //pid = 100
-if(pid == 0){
-
-int i;
+	while (i);
+	system("clear");
+	char c;
+	int i = 1;
+	int k;
 	int l;
-	char k;
+	int j = 0;
+	char test[TAILLE_MAX];
 	FILE* fichier = NULL;
-	fichier = fopen("1.pbm", "r");
+	fichier = fopen("2.pbm", "r");
 	if (fichier != NULL)
 	{
-		for(i=0;i!=2;i++)
+		while (fgets(test, TAILLE_MAX, fichier) != NULL);
+		{
+			j++;
+		}
+		ioctl(0,TIOCGWINSZ,&win);
+		lig = win.ws_row;
+		j = lig/2-j/2;
+		for(i=0;i!=j;i++)
+		{
+			printf("\n");
+		}
+		fseek(fichier,0,SEEK_SET);
+		for(i=0;i!=3;i++)
 		{
 			fgets(chaine, TAILLE_MAX, fichier);
 		}
-		fgets(chaine, TAILLE_MAX, fichier);
-		l=strlen(chaine);
-//		for(i=0;i!=l;i++)
-//		{
-//			if (chaine[i]==' ')
-//			{
-//				k=chaine[i+1]
-//			}
-//		}
 		while (fgets(chaine, TAILLE_MAX, fichier) != NULL)
 		{
 			l=strlen(chaine);
@@ -81,11 +86,8 @@ int i;
 			afficher();
 		}
 		fclose(fichier);
-	}	
-}
-else{
-	printf("alors");
-	wait(NULL);
-}
-return 0;
+	}
+	//getch();
+	}
+	return 0;
 }
